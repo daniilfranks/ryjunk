c06cb7429319510b66f82951c285a6c85c6be997
Fredrik Svärd <fredrik.svard@gmail.com>
2015-10-16 23:51:18 +0200

    Threading

--- tutorial/files/list_files.rb
+++ tutorial/files/list_files.rb
@@ -2,4 +2,50 @@
 
 ll = Dir.glob(['*.rb', '*.gz'])
 
-print ll
+puts ll
+
+require 'singleton'
+class Nisse
+
+	include Singleton
+
+    attr_writer :freq
+
+	def initialize
+		puts 'Hej från Nisse', self.object_id
+	end
+
+	def run
+		while true 
+	      puts 'Hej igen från Nisse',self.object_id
+	      sleep @freq 
+	    end
+	end
+end
+
+class Janne
+
+#  @@nisse ||= Nisse.instance
+
+  @@nisse = Nisse.instance
+  @@nisse.freq = 2
+  def initialize
+  	trad = Thread.new { @@nisse.run }
+  	puts 'Hej från Janne'
+  	sleep 10
+  	Thread.kill(trad)
+    puts trad.alive?
+  end
+
+end
+
+require 'thread'
+
+nn = [Janne.new, Janne.new]
+
+
+all  = nn.each do | x | Thread.new { x } end
+
+
+sleep 1
+
