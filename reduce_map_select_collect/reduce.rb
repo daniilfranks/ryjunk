e1cb66fd0bf6c6c9f55549ee994dbce314564ae0
Fredrik Svärd <fredrik.svard@gmail.com>
2015-11-07 22:37:00 +0100

    More on reduce and stuff

--- reduce_map_select_collect/reduce.rb
+++ reduce_map_select_collect/reduce.rb
@@ -1,5 +1,37 @@
 
+# http://www.eriktrautman.com/posts/ruby-explained-map-select-and-other-enumerable-methods
+# http://ruby-doc.org/core-2.0.0/Enumerable.html#method-i-none-3F
 
-r = [1,2,3,4].reduce do | x | x end
+
+puts 'each'
+[1,2,3].each do | num | print "#{num}!" end
+# select
+
+puts 'select'
+my_array = [1,2,3,4,5,6,7,8,100]
+my_select =  my_array.select do | item | item%2 == 0 end
+
+puts my_select
+
+
+# collect == map!
+puts 'collect/map'
+my_collect = my_array.collect { |num|  num**2 }
+
+puts my_collect
+
+
+# inject == reduce
+puts 'reduce/inject'
+
+r = [1,2,3,4].reduce do | total, item | total + item end
 
 puts r
+
+# none?
+
+# any? |
+
+# all? { |e|  }
+
+# find
