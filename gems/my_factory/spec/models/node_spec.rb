d4990ece50358f6710c2a1307f2b0510b673225f
fredrik svard <fredrik.svard@gmail.com>
2015-11-29 14:53:56 +0100

    factory girl example

---
+++ gems/my_factory/spec/models/node_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Node do
+  describe 'instantiation' do
+    let!(:node) {build(:node)}
+
+    it 'instantiates a node' do
+      expect(node.class.name).to eq("Node")
+    end
+  end
+end
