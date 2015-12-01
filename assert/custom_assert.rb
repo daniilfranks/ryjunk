92862913d15257dc45dd0b1c83b1356317ae0fac
Fredrik Svärd <fredrik.svard@svenskaspel.se>
2015-12-01 14:48:59 +0100

    custom assert and assert

---
+++ assert/custom_assert.rb
@@ -0,0 +1,36 @@
+
+
+  require 'test/unit'
+
+  class TC_Custom < Test::Unit::TestCase
+
+    def assert_eq_im(expected, number)
+      assert_block("Expected #{number.imaginary} to be equal to #{expected.imaginary}") do
+       number.imaginary == expected.imaginary
+      end
+    end
+
+    def assert_neq_re(expected, number)
+      assert_block("Expected #{number.real} to be equal to #{expected.real}") do
+       number.real != expected.real
+      end
+    end
+
+    def setup
+      @c1 = Complex(1,2)
+      @c2 = Complex(3,2)
+    end
+
+    def test_object
+      assert_instance_of(Complex, @c1)
+    end 
+
+    def test_eq_im
+      assert_eq_im(@c1, @c2)
+    end 
+
+    def test_neq_re
+      assert_neq_re(@c1, @c2)
+    end 
+
+  end
