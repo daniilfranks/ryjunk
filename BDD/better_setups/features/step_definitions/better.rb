5b0018465b1d3b40aabf780a9c73f6970594ef51
fredrik svard <fredrik.svard@gmail.com>
2016-02-05 23:36:54 +0100

    Clean up

--- BDD/better_setups/features/step_definitions/better.rb
+++ BDD/better_setups/features/step_definitions/better.rb
@@ -1,27 +1,8 @@
-#require 'sham'
-#require 'forgery'
-require "observer"
 require 'machinist'
 require 'faker'
 require "test/unit/assertions"
 include Test::Unit::Assertions
 
-
-class Nisse
-  include Observable
-
-  def initialize object
-    @obj = object
-  end
-
-  def run
-
-  	#puts 'trying to notify'
-  	sleep 6
-  	changed
-  	notify_observers(@obj)
-  end
-end
 
 
 # http://itsignals.cascadia.com.au/?p=30
@@ -48,35 +29,23 @@ class MyObject
 end
 
 # For all Objects
-class Object
- extend Machinist::Machinable
+#class Object
+# extend Machinist::Machinable
 
- def self.blueprint_class
-   Machinist::Blueprint
- end
-end  
+# def self.blueprint_class
+#   Machinist::Blueprint
+# end
+#end  
 
-Given(/^we can setup an arbitrary user$/) do
-  # Shams
-  # We use forgery to make up some test data
-  
-  @my_array = {}
-  @my_array[:pilluta] = false
+Before do
 
-  class Orvar ;attr_accessor:pilutta ;def initialize ; @pilutta = false ;end end
-
-  @object = Orvar.new
-
-  @nisse = Nisse.new @object
-  init_observer(@nisse)
- 
- 
   # Blueprints
   # http://www.rubydoc.info/github/stympy/faker/frames
- ExternalUser.blueprint do
- 	organisation {Faker::Company.name}
- 	description  {Faker::Lorem.sentence}
- 	contact      {Faker::Name.name}
+   ExternalUser.blueprint do
+ 	
+    organisation {Faker::Company.name}
+    description  {Faker::Lorem.sentence}
+    contact      {Faker::Name.name}
     email        {Faker::Internet.email}
     phone        {Faker::PhoneNumber.cell_phone}
     website      {Faker::Internet.url}
@@ -91,12 +60,17 @@ Given(/^we can setup an arbitrary user$/) do
     name { Faker::Internet.email }
   end
 
+
+end
+
+
+Given(/^we can setup an arbitrary user$/) do
+
   @obj = MyObject.make(3)
   @test = MyObject.make
   #@test.should be_a(MyObject)
   #test = MyObject.make_unsaved(:date => Time.now)
 
-  #pending # Write code here that turns the phrase above into concrete actions
   user = ExternalUser.make
 
   user.enabled = false
@@ -108,26 +82,14 @@ When(/^I make an update$/) do
   #puts @obj.inspect
   @old_email = @obj.first.email
   @obj.first.email = "nisse.hult@gmail.com" 	
-  #pending # Write code here that turns the phrase above into concrete actions
-  
-  # Get ready for a async callback!
-  @pilutta = false
-  @thread = Thread.new { @nisse.run }
 
 end
 
 Then(/^I should see the difference$/) do
   assert_not_equal @old_email, @obj.first.email 
 
-  Timeout::timeout (10) {
-    until @object.pilutta do
-      sleep 0.05
-    end
-  }
-  
-  @thread.join
-
-
   puts "-----The end!-----------"	
-  #pending # Write code here that turns the phrase above into concrete actions
+  puts @obj
 end
+
+
