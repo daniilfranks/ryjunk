3e3dd2873382bbca8bf64bedc6b5d686bdaace2c
Fredrik Svärd <fredrik.svard@gmail.com>
2016-03-12 10:03:41 +0100

    nice stuff

--- object_stuff/my_tp.rb
+++ object_stuff/my_tp.rb
@@ -1,29 +1,43 @@
 require 'pp'
 
 
-tracer = TracePoint.new(:call,:return) do |tp|
-  if tp.method_id != :checkRep
-    tp.self.send(:checkRep)
-  end
+invariants = TracePoint.new(:call,:return) do |tp|
+
+
+  if tp.method_id != :checkRep and not 
+    (tp.method_id == :initialize and tp.event == :call)  
+   
+      tp.self.send(:checkRep)
+  end  
 end
 
 class MyClass
+
+  def initialize
+    puts 'initialize'
+    puts self.class.name
+  end
+
   def my_method
     puts 'my_method'
     42
   end
 
+  private
   def checkRep
     puts 'checkRep'
     51
   end
 end
 
-tracer.enable
+invariants.enable do 
 
-foo = MyClass.new
+  foo = MyClass.new
+  foo.my_method
+end
 
+doo = MyClass.new
 
-foo.my_method
+doo.my_method
 
 
