a063130fd46a409f9b88602b041cde7c560976a7
Fredrik Svärd <fredrik.svard@gmail.com>
2016-05-30 20:53:48 +0200

    Refactored extra_

--- games/about_extra_credit.rb
+++ games/about_extra_credit.rb
@@ -15,36 +15,39 @@ class Player
   attr_reader :finished, :name, :score
 
   def initialize name
-  	@name = name
-  	@score = 0
-  	@finished = false
-  end	
+    @name = name
+    @score = 0
+    @finished = false
+  end
 
   def save_score score
     @score+= score
-  
+
     @finished = true if @score >= 3000
   end
 end
 
 class Greeds
   def initialize dice_set = nil
-  	@dice_set = dice_set
-  	@result = nil
-  	@no_of_rolls = 0
+    @dice_set = dice_set
+    @result = nil
+    @no_of_rolls = 0
   end
 
-  def play 
-  	nisse =  @dice_set.roll(5)
+  def play
+    nisse =  @dice_set.roll(5)
     @result = score2(@dice_set.roll(5))
   end
 end
 
+class Cycle
 
-$no = 0
-def get_cycle players
-   $no+=1
-   players[$no % players.size]
+  @@no = 0
+
+  def self.get_cycle list
+    @@no+=1
+    list[@@no % list.size]
+  end
 
 end
 
@@ -52,32 +55,28 @@ end
 if __FILE__ == $PROGRAM_NAME
 
   players = [Player.new("Janne"), Player.new("Olle"), Player.new("Sussi")]
- 
+
   dice = DiceSet.new
   greed = Greeds.new(dice)
-        
-  count = 0
-        
-  while true
-            
-    player = get_cycle(players)
-            
 
-    break if player.finished
+  count = 0
+
+  while true
+
+    player = Cycle.get_cycle(players)
+
     # Everyone had a chance after ther first scored 30000
-               
-            
+    break if player.finished
+
     player.save_score(greed.play())
     count+= 1
-            
 
-            
   end
 
   puts "count:", count
-  
-  players.each do | player | 
+
+  players.each do | player |
     puts "player: #{player.name}, score: #{player.score}"
-  end	
+  end
 
 end
