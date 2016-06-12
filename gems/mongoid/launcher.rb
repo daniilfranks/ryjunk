3edb9758565f2a267f2b76db58b2fd485c6cb9cf
fredrik svard <fredrik.svard@gmail.com>
2016-06-12 23:17:43 +0200

    More on this

--- gems/mongoid/launcher.rb
+++ gems/mongoid/launcher.rb
@@ -24,8 +24,34 @@ end
 
 if __FILE__ == $PROGRAM_NAME
 
-launcher = Launcher.new(resulter: Match)
-launcher.save
+status_tr = {nil => "exception", false => 'normal exit', "aborting" => 'abort', 
+             "sleep" => "sleep", "run" => "run"}
+
+status_tr.default= "unknown exit"
+threads = []
+begin 
+
+  threads << Thread.new { raise("die now") }
+  #threads << Thread.new { Thread.stop }
+  threads << Thread.new { Thread.exit 2 }
+   threads << Thread.new { Thread.exit  }
+  #threads << Thread.new { exit 2 } 
+  threads << Thread.new { raise Exception }
+  threads.each { |thr| thr.join }
+
+rescue => e
+  #puts e.inspect
+end
+
+threads.each { |thr| puts "dd #{status_tr[thr.status]}" }
+puts "*"*40
+threads.each { |thr| puts "dd #{thr.status}" }
+# Join and check how the thread did exit
+
+
+
+#launcher = Launcher.new(resulter: Match)
+#launcher.save
 
 
 
