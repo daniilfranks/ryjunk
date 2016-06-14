0515092d64f2e596319a89ec56477f306081f291
fredrik svard <fredrik.svard@gmail.com>
2016-06-14 22:17:12 +0200

    more on this

--- gems/activesupport/test_some.rb
+++ gems/activesupport/test_some.rb
@@ -2,7 +2,21 @@
 require  'active_support/all'
 #require 'active_support/core_ext/object/try.rb'
 
- 
+#http://guides.rubyonrails.org/active_support_core_extensions.html
+
+class User
+
+  attr_reader :first_name
+
+  def initialize
+    @first_name = 'Olle'
+#    @first_name = nil
+  end
+
+end 
+
+@user = User.new
+
 puts 3.days.ago
 
 puts 3.months.ago
@@ -16,4 +30,6 @@ puts 3.months.from_now
 
 puts 3.years.from_now
 
+
+print @user.try(:first_name)
 
