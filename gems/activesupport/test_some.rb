d51e50c639f18a77fa6678c15479311d7197af7b
fredrik svard <fredrik.svard@gmail.com>
2016-06-14 22:05:09 +0200

    more on time

--- gems/activesupport/test_some.rb
+++ gems/activesupport/test_some.rb
@@ -2,4 +2,18 @@
 require  'active_support/all'
 #require 'active_support/core_ext/object/try.rb'
 
+ 
 puts 3.days.ago
+
+puts 3.months.ago
+
+puts 3.years.ago
+
+
+puts 3.days.from_now
+
+puts 3.months.from_now
+
+puts 3.years.from_now
+
+
