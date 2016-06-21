2ed95d59e43a8649b46da61f9b0e1da4d4c52e0e
fredrik svard <fredrik.svard@gmail.com>
2016-06-21 05:44:05 +0200

    Added color

--- gems/simple-tracer/hej.rb
+++ gems/simple-tracer/hej.rb
@@ -1,3 +1,18 @@
-
+require 'simple-tracer'
 # http://www.blackbytes.info/2012/06/ruby-tracing/
-puts 'hej'
+
+def nisse
+  puts 'hej'
+end
+
+
+
+
+
+if __FILE__ == $PROGRAM_NAME
+
+  nisse()
+
+end
+
+
