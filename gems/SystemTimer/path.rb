561bab8f214710ab76469e3797a1c157a106874b
fredrik svard <fredrik.svard@gmail.com>
2016-07-07 22:42:53 +0200

    smarter

--- gems/SystemTimer/path.rb
+++ gems/SystemTimer/path.rb
@@ -4,4 +4,7 @@ idx = Dir.pwd.split('/').index('gems')
 p  Dir.pwd.split('/')[0..idx].join('/')
 
 
-p Dir.pwd.split('/')[0..lambda { Dir.pwd.split('/').index('gems')}.call].join('/')
+
+p Dir.pwd.split('/')[0..lambda { Dir.pwd.split('/').index('gems')}.()].join('/')
+
+
