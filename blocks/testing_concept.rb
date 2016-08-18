6b04967bb92537a9c891ee2838d323ee2e63be7b
Fredrik Svärd <fredrik.svard@gmail.com>
2016-08-18 23:03:10 +0200

    even better

--- blocks/testing_concept.rb
+++ blocks/testing_concept.rb
@@ -2,8 +2,8 @@
 
 class NissePelle
 
-  def initialize
-    @nisse = {hemma: 'orvar'}
+  def initialize hut
+    @nisse = {hemma: hut }
   end
 
   def mymy
@@ -12,14 +12,34 @@ class NissePelle
 end
 
 
+class Holder
+
+  def initialize lista
+    @lista = lista
+  end
+
+  def each 
+     @lista.each { | e | yield e } 
+  end
+
+end
+
 
 
 if __FILE__ == $PROGRAM_NAME
 
-  nisse_pelle = NissePelle.new
+  nisse_pelle = NissePelle.new 'hepp'
 
   nisse_pelle.mymy do | hej |
     puts hej
+  end
+
+  om = Holder.new([NissePelle.new('och?'), NissePelle.new('nej!')])
+
+  om.each do | e |
+    e.mymy do | x |
+      puts x
+    end 
   end
 
 end
