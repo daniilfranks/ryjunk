b36c4b09d9fc61f23c8236e4f53c953442380dc4
fredrik svard <fredrik.svard@gmail.com>
2016-09-10 16:19:33 +0200

    minitest

---
+++ BDD/better_setups/features/support/env.rb
@@ -0,0 +1,15 @@
+
+require 'minitest/spec'
+
+class MinitestWorld
+  include Minitest::Assertions
+  attr_accessor :assertions
+
+  def initialize
+    self.assertions = 0
+  end
+end
+
+World do
+  MinitestWorld.new
+end
