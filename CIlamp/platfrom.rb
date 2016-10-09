801f816072bc9c02e73688ea740597fcc1bf7b1f
Fredrik Svärd <fredrik.svard@gmail.com>
2016-10-09 21:40:15 +0200

    More on this

--- CIlamp/platfrom.rb
+++ CIlamp/platfrom.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 
 module OS
   def OS.windows?
@@ -9,21 +11,16 @@ module OS
   end
 
   def OS.unix?
-    not OS.windows?
+    !OS.windows?
   end
 
   def OS.linux?
     OS.unix? and not OS.mac?
   end
 
-  def OS.get_platform
-    nisse = (OS.mac?)? :Mac:(OS.linux?)? :Linux : :Windows
+  def get_platform
+    nisse = (OS.mac?)? "Mac":(OS.linux?)? "Linux": "Windows"
   end
-
-  def OS.ng_get_platform
-    # something with Hash  { OSmac? => :Mac, etc?}, possible? or case?
-  end
-
 end
 
 class ListAPI
@@ -31,99 +28,44 @@ class ListAPI
   def initialize
   end
 
-  def play_mp3 file, mode
+  def get_cwd
     raise NotImplementedError
   end
 end
 
-# TODO: how to use a better way than abs apth with daemons?
-PATH = "/Users/fabv/ehea-fabv-project/usbLamp/blinky/"
-
 class ListAPILinux< ListAPI
 
   def initialize
-    super()
   end
 
-  def play_mp3(file = "cartoon-computer_ds.mp3", mode)
+  def get_cwd
 
-    case mode
-      when "success"
-        system("afplay #{PATH}success.mp3")
-      when "building"
-        system("afplay #{PATH}buildning.mp3")
-      when "failure"
-        system("afplay #{PATH}failure.mp3")
-      else
-        system("afplay #{PATH}others.mp3")
-    end
+    return FileUtils.pwd()
   end
 
-  def old_play_mp3(file = "cartoon-computer_ds.mp3", mode)
-
-    puts "->>>#{@config}"
-
-    case mode
-      when "success"
-        system("afplay success.mp3")
-      when "building"
-        system("afplay buildning.mp3")
-      when "failure"
-        system("afplay failure.mp3")
-      else
-        system("afplay others.mp3")
-    end
-  end
 end
 
 class Platform
   include OS
 
-  def initialize
-
-  operativ = {:Linux => ListAPILinux.new, :Mac => ListAPILinux.new, :Windows => ListAPILinux.new }
+  def initialize 
+  operativ = {'Linux' => ListAPILinux.new, 'Mac' => ListAPILinux.new, 'Windows' => ListAPILinux.new }
  
 
     @here = operativ.fetch(get_platform)  
   end
 
-  def play_mp3 mode
-    @here.play_mp3(mode=mode)
+  def get_cwd
+    @here.get_cwd
   end
 
 end
 
-class TheAPI
-
-  def self.pick(platform)
-
-    nn = { :Linux => ListAPILinux.new, :Mac => ListAPILinux.new }
-    nn.default = 'Nej!!'
-    return nn[platform]
-  end
-
-end
-
-class NewPlatform
-
-  def initialize
-
-    @the_api = TheAPI.pick(OS.get_platform)
-  end
-
-end
-
-if __FILE__ == $PROGRAM_NAME
-
-  #config = {"sounds" => { 'success' => "success.mp3" } }
+if __FILE__ == $0
   
-  #platform = Platform.new
-  #platform = Platform.new
+  platform = Platform.new
 
-  #platform.play_mp3(mode="success")
+  puts platform.get_cwd
 
-  @the_api = TheAPI.pick(OS.get_platform)
-
-  @the_api.play_mp3(mode="building")
-
+  
 end
