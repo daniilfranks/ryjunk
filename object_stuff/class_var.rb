409fa692b8424103bb51d83f4362b47ee1c4ff37
Fredrik Svärd <fredrik.svard@gmail.com>
2016-10-21 20:28:37 +0200

    the solution

---
+++ object_stuff/class_var.rb
@@ -0,0 +1,43 @@
+
+class Nisse
+  def initialize
+    @@orre ||= 42
+  end
+end
+
+
+class Olle < Nisse
+
+  def initialize
+    super()
+    puts "from Olle: #@@orre"
+  end
+end
+
+
+class Hutte < Nisse
+
+  def initialize
+    super()
+    puts "from Huttee: #@@orre"
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+
+  puts 'the start'
+  olle = Olle.new
+
+  hutte = Hutte.new
+
+  nu = Time.now
+  sleep 2
+  sen = Time.now
+ 
+   puts sen-nu
+   puts (sen-nu).class
+  
+
+end
+
