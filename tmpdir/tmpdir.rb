bb75cfa49e5e987867d7951a51cacc442f202275
Fredrik Svärd <fredrik.svard@gmail.com>
2016-10-31 09:40:32 +0100

    tmpdir

--- tmpdir/tmpdir.rb
+++ tmpdir/tmpdir.rb
@@ -1,7 +1,20 @@
-
-
-nn = Dir.tmpdir
-
+require 'tempfile'
+require 'fileutils'
+#nn = Dir.tmpdir
 #nn =Dir.mktmpdir(nil, "/var/tmp") {|dir| dir is "/var/tmp/d..." }
+#puts nn
+
+=begin
+Dir.mktmpdir do |dir|
+  puts "My new temp dir: #{dir}"
+end
+=end
+
+nn =Dir.mktmpdir() 
 
 puts nn
+
+FileUtils.remove_dir(nn)
+
+FileUtils.remove_dir(nn)
+
