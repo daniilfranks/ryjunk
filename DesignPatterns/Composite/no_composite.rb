e1edc2b31c65f061ecfd540ee26c1eb11cf42243
Fredrik Svärd <fredrik.svard@gmail.com>
2016-11-04 13:49:45 +0100

    more on this

--- DesignPatterns/Composite/no_composite.rb
+++ DesignPatterns/Composite/no_composite.rb
@@ -1,53 +1,6 @@
 
-# VILO RDY 100.dtd
-def get_dtd_msg_type(name)
-
- filename = File.basename(name)
- olle = filename.match(/(?<game>[\w]{4})(?<msg>[\w]{3})(?<rrr>[\w\d\.]+)/)
- puts  olle[:msg]
- puts  olle[:game]
- return olle[:msg]
-end
-
-class FileFromSystem
-
-  attr_reader :datum, :msg
-
-  def initialize filename
-    @filename = filename
-    parse_filename
-  end
-
-  def parse_filename
-    nisse = @filename.match(/(?<datum>[\d]{7})(?<game>[\w]{4})(?<msg>[\w]{3})(?<src>[\w]{3})(?<dst>[\w]{3})(?<rrr>[\w\d\.]+)/)
-    @datum = nisse[:datum]
-    @msg = nisse[:msg]
-  end
-
-  def <=> other
-  end
-
-end
-
-class FilesFromSystem
-
-  attr_reader :files
-
-  include Enumerable
-
-  def initialize
-    @files = []
-  end
-  
-  def<<(item)
-    @files << FileFromSystem.new(item)
-  end
-
-  def each
-  end
-
-end
-
+# TODO: Make a generic class and and ReaderWriter (scp, ftp, http, networkdisk) to put/get files,
+# store in an array
 
 class DummyCarrier
 
@@ -79,84 +32,129 @@ class Accessor
 
 end
 
-if __FILE__ == $PROGRAM_NAME
 
+###############################################################################
 
-# TODO: Make a generic class and and ReaderWriter (scp, ftp, http, networkdisk) to put/get files, 
-# store in an array
-
-
-
-# Fetch file list on current  (RDY, SSC, etc ...) timestamp on file or timestamp on filename?
-
-# TODO: better filter for specific file ...
-
-#nn = Dir.glob(['*.rb', '*.gz'])
-nn = Dir.glob(['*.xml'])
-
-files_from_system = FilesFromSystem.new
-
-
-
-Dir.glob(['*.xml']) do | file |
-  files_from_system << file
+def get_dtd_msg_type(name)
+ filename = File.basename(name)
+ olle = filename.match(/(?<game>[\w]{4})(?<msg>[\w]{3})(?<rrr>[\w\d\.]+)/)
+ return olle[:msg]
 end
 
-dtd_files_from_system = {}
 
-
-# TODO: Select all DTDs and add them to a dict with message type as key!
-
-
-dtd_paths = '/Users/fabv/ryjunk/DesignPatterns/Composite/dtds'
-
-puts dtd_paths
-
-#Dir.glob(['*.dtd']) do | file |
-Dir.glob(["#{dtd_paths}/**/*.dtd"]) do | file |
-  dtd_type = get_dtd_msg_type(file)
-  dtd_files_from_system[dtd_type] = file
-end
-
-puts dtd_files_from_system
-
-sorted = nn.sort do | a, b  |  File.birthtime(a) <=> File.birthtime(b) end
-
-# latest file !!
-xmlfile =  sorted.last
-
-# Validate file
-
-require 'libxml'
-
-#dtd_name = xmlfile.gsub("xml", "dtd")
-dtd_name = "#{dtd_files_from_system[files_from_system.files.first.msg]}"
-
-puts dtd_name
-
-path='./'
-
-#raise "No DTD found for this XML-type #{xmlfile}" unless File.exist?("#{path}#{dtd_name}")
-
-    include LibXML
-
-#dtd = XML::Dtd.new(File.read("#{path}#{dtd_name}"))
-dtd = XML::Dtd.new(File.read("#{dtd_name}"))
-doc = XML::Document.file("#{path}#{xmlfile}")
-doc.validate(dtd)
-
-# Verify file
-
+require 'date'
 require 'nokogiri'
 
-doc = File.open(xmlfile) { |f| Nokogiri::XML(f) }
+class FileFromSystem
 
-msg =  doc.at_xpath 'vilordy/header/type'
+  include Comparable
+  attr_reader :date_content, :msg, :filename
 
-puts msg.text
-puts files_from_system.files.first.msg
+  def initialize filename
+    @filename = filename
+    parse_filename
+  end
 
-# Parse filename and compare with content in file...
+  def parse_filename
+    nisse = @filename.match(/(?<datum>[\d]{7})(?<game>[\w]{4})(?<msg>[\w]{3})(?<src>[\w]{3})(?<dst>[\w]{3})(?<rrr>[\w\d\.]+)/)
+    @date_content = Date.strptime(nisse[:datum], '%G%V%u')
+    @msg = nisse[:msg]
+  end
+
+  def <=> other
+     self.date_content <=> other.date_content
+  end
+
+
+  # TODO: Uniq method for every type of msg?
+
+  def verify_content
+
+    # TODO: Use search instead of xpath, not use absolute path...
+
+    doc = File.open(@filename) { |f| Nokogiri::XML(f) }
+    msg =  doc.at_xpath "vilo#{@msg.downcase}/header/type"
+
+    dtx = doc.at_xpath "vilo#{@msg.downcase}/header/created"
+    dt = Date.parse(dtx.text)
+
+    return (msg.text == @msg and @date_content == dt)
+  end
+
+end
+
+class FilesFromSystem
+
+  attr_reader :files
+
+  include Enumerable
+
+  def initialize
+    @files = []
+  end
+  
+  def<<(item)
+    @files << FileFromSystem.new(item)
+  end
+
+  def old_each
+    @files.each { | f | f }
+  end
+
+  def each(&block)
+    @files.each(&block)
+  end
+
+end
+
+
+# latest file !!
+#nn = Dir.glob(['*.xml', '*.gz'])
+#sorted = nn.sort do | a, b  |  File.birthtime(a) <=> File.birthtime(b) end
+#xmlfile =  sorted.first
+
+def validate_xml dtd_files_from_system, files_from_system
+
+  require 'libxml'
+  include LibXML
+
+  sorti = files_from_system.sort
+  xmlfile = sorti.first
+
+  # Find DTD for given XML!
+  raise "No DTD found for this XML-type #{xmlfile.filename}" unless dtd_files_from_system.has_key?(xmlfile.msg)
+  dtd_name = "#{dtd_files_from_system[xmlfile.msg]}"
+
+
+  path ='./'
+  dtd = XML::Dtd.new(File.read("#{dtd_name}"))
+  doc = XML::Document.file(xmlfile.filename)
+  doc.validate(dtd)
+
+  puts "validate OK!"
+
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+
+# TODO: Filter for RDY or SNW or other message files ....
+puts "---Find XML files ..."
+files_from_system = FilesFromSystem.new
+xml_path = '.'
+Dir.glob(["#{xml_path}/**/*.xml"]) { | file | files_from_system << file }
+
+puts "---Find DTDs..."
+dtd_files_from_system = {}
+dtd_paths = './dtds'
+Dir.glob(["#{dtd_paths}/**/*.dtd"]) { | file |  dtd_files_from_system[get_dtd_msg_type(file)] = file  }
+
+puts "---Validate file"
+validate_xml dtd_files_from_system, files_from_system
+
+puts "---Verify file"
+
+raise "Verification failed ..."  unless files_from_system.files.first.verify_content
 
 
 end
