7745f24a31d847167c2e8f8e2811069e62f0c05d
Fredrik Svärd <fredrik.svard@gmail.com>
2016-11-23 09:03:23 +0100

    more on date

--- regexp/regexp.rb
+++ regexp/regexp.rb
@@ -11,9 +11,20 @@ puts nn[:msg]
 puts nn[:src]
 puts nn[:dst]
 
+# 2016473
 
-#Date.strptime('2001-W05-6', '%G-W%V-%u')
+dt1 = Date.strptime('2016-W47-3', '%G-W%V-%u')
 
 dt = Date.strptime(nn[:datum], '%G%V%u')
 
-puts dt
+puts "no #{dt}"
+
+puts "no1 #{dt1}"
+
+
+puts '-' * 80
+
+
+dt2 = DateTime.now # ('2016-11-23', '%G-W%V-%u')
+puts dt2.strftime('%G%V%u')
+
