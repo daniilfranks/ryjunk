787eb10adb23e523c2feec8380a489059260974d
fredrik <fredrik.svad@gmail.com>
2016-12-06 22:41:38 +0100

    more on ftp

---
+++ ftp/new_ftp_sunet.rb
@@ -0,0 +1,69 @@
+require 'net/ftp'
+require 'fileutils'
+#anonymous
+
+class FetchTask
+
+  include Enumerable
+
+  def self.init args
+    puts 'init'
+    @@args = self.defaults.merge(args)
+    @@ftp = Net::FTP.open(@@args[:server])
+  end
+
+  def self.ls
+    self.init(args = {})
+    puts 'ls'
+    @@ftp.login(user = @@args[:user], passwd = @@args[:pwd])
+    @@ftp.chdir(@@args[:rpath])
+    @@ftp.list()
+  end
+
+  def self.doit(args={})
+
+    @@args = defaults.merge(args)
+
+    FileUtils.mkdir(@@args[:path]) unless File.exist?(@@args[:path])
+
+    Net::FTP.open(@@args[:server]) do |ftp|
+      ftp.login(user = @@args[:user], passwd = @@args[:pwd])
+      ftp.chdir(@@args[:rpath])
+      files = ftp.list() #{ args[:suffix] }
+      filtered = files.select { | file | file.include?('pdf') }  
+#      fetch_files = files.collect { | file | "#{file.split(' ').last}" }  
+      puts filtered
+#      filtered.each { | file | ftp.getbinaryfile(remotefile=file , localfile = "#{@@args[:path]}/#{file}") }
+
+    end
+
+  end
+
+  def self.defaults
+    {
+      server: 'ftp.sunet.se', user: 'anonymous', pwd: '',
+#      rpath: 'mirror/archive/ftp.sunet.se/pub/Linux/distributions/bifrost/download/logo/',
+      rpath: '/mirror/archive/ftp.sunet.se/pub/Linux/distributions/bifrost/doc/',
+#      suffix: ['*.png', '*.xml', '*.zip'],
+      suffix: ['*.pdf'],
+      path:  'tmp'
+    }
+  end
+
+  def self.remove_dir
+    #rpath = defaults[:path]
+    FileUtils.remove_dir(defaults[:path])
+  end
+
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+
+  #FetchTask.doit()
+
+#  FetchTask.remove_dir
+
+  puts FetchTask.ls()
+
+end
