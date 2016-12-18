1ab6241e912f3808cf612c45fc8f9ae5c5a45974
Fredrik Svard <fredrik.svad@gmail.com>
2016-12-18 11:04:18 +0100

    First attempt

---
+++ projects/muraltr/get_from_tw.rb
@@ -0,0 +1,35 @@
+
+
+
+
+
+if __FILE__ == $PROGRAM_NAME
+
+  # --- Steg 0 ---
+  # login to twitter
+
+  # download tweets from muralgranskaren
+
+  # save all words in a dictionary/set
+
+  # make a json structure with  muraltr = {'dettha' : 'dummy', 'merha' : 'dummy' }
+
+  # Spara som fil jsonfil.
+
+
+  # --- Steg 1 ---
+
+  # titta igenom filen och oversatt alla ord.
+
+  # kasta om sa att key blir value och value blir key
+
+  # spara i jsonfil
+
+  # skapa skript som oversatter om ordet finns i dictionaryt(jsonfilen)
+
+  
+  # --- Steg 2 ---
+
+  # Ha en hantering dar man kan inkrementellt lagga till ord fran befintlig oversatting?
+
+end
