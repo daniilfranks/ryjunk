569c33af5e0c1261a734285425847c6a75d42975
Fredrik Svard <fredrik.svad@gmail.com>
2016-12-18 21:30:37 +0100

    tw

---
+++ gems/twitter/testa.rb
@@ -0,0 +1,24 @@
+require 'twitter'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = YOUR_CONSUMER_KEY
+  config.consumer_secret     = YOUR_CONSUMER_SECRET
+  config.access_token        = YOUR_ACCESS_TOKEN
+  config.access_token_secret = YOUR_ACCESS_SECRET
+end
+
+
+#tweets = client.user_timeline('rubyinside', count: 20)
+tweets = client.user_timeline('Muralgranskaren', count: 20)
+
+tweets.each { |tweet| puts tweet.full_text }
+
+
+require 'yaml'
+require 'json' 
+# ... rest of the code here ...
+ 
+#File.write('tweets.yml', YAML.dump(tweets))
+#twts = JSON.pretty_generate(tweets)
+File.write('tweets.json', JSON.dump(tweets))
+#File.write('tweets.json', JSON.pretty_generate(tweets))
